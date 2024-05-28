94b74a4ede2697c2c58c72ed2778e7794f18e43f
Helvola <y2794015616@outlook.com>
2024-05-28 22:03:48 +0800

    all

---
+++ test/test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+
+#define maxn 1e7;
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+clock_t start,stop;
+double runtime(){return ((double)(stop - start))/CLOCKS_PER_SEC;}
+
+const int N = 1e6 + 10;
+
+int main() {
+    ios::sync_with_stdio(0),cin.tie(0);
+    start = clock();
+    stop = clock();
+    
+cout << "time = " << runtime() << endl;
+return 0;
+
+}
